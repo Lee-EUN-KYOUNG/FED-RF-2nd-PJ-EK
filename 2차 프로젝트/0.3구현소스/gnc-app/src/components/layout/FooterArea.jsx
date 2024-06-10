e90e7490d38b096c43df9e150e3b9793e37c2fde
EUNKYOUNG <amurca@naver.com>
2024-06-10 16:54:16 +0900

    11111

---
+++ 2차 프로젝트/0.3구현소스/gnc-app/src/components/layout/FooterArea.jsx
@@ -0,0 +1,23 @@
+// 하단 메뉴 데이터 불러오기
+//import { bmData } from "../data/bmenu";
+
+// 하단 영역 CSS 불러오기
+//import "../../css/footer_area.scss";
+
+export default function FooterArea() {
+  //// 코드 리턴구역 //////////////
+  return (
+    <footer className="info">
+      <ul>
+        <li>
+        </li>
+        <li>
+          {/* 하단링크박스 */}
+          <ol className="bmenu">
+          </ol>
+        </li>
+        <li>© & ™ </li>
+      </ul>
+    </footer>
+  );
+} /////////// FooterArea /////////////////////
