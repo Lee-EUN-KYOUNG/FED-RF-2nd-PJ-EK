1c35de1981235918b853c2d2532c9526013447b5
EUNKYOUNG <amurca@naver.com>
2024-06-17 18:18:02 +0900

    1111

--- 2차 프로젝트/0.3구현소스/gnc-app/src/components/modules/Notice.jsx
+++ 2차 프로젝트/0.3구현소스/gnc-app/src/components/modules/Notice.jsx
@@ -1,30 +1,36 @@
 // 공지 상단 메뉴
-import React from 'react';
-import IntroPage from '../modules/IntroPage';
+import React from "react";
+import IntroPage from "../modules/IntroPage";
 
 // CSS 연결
 import "../../css/pivot/notice.scss";
 
 //////////// 코드구역
 function Notice(props) {
-    return (
-        <>
-        <section className="notice-box">
-            <div className="notice-bar">
-            {/* 공지바 영역 */}
-            <h1>
-            <p>
-            NOITCE
-            </p>
-            </h1>
-            </div>
-            <div className="intro-page">
-            {/* 인트로 메인 이미지 영역 */}
-            <IntroPage />
-            </div>
-        </section>
-        </>
-    );
+  return (
+    <>
+      <section className="notice-box">
+        <div className="notice-bar">
+          {/* 공지바 영역 */}
+          <h1>
+            <p>NOITCE</p>
+          </h1>
+        <div className="notice-main">
+          <h2>NEW!</h2>
+          <ul className="notice-text">
+            <li>
+                <p>GNC 미디어 에이전시 홈페이지 리뉴얼!</p>
+            </li>
+          </ul>
+        </div>
+        </div>
+        <div className="intro-page">
+          {/* 인트로 메인 이미지 영역 */}
+          <IntroPage />
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Notice;
