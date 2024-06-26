ed3147dce2a2691c7d76214dd0524ac738f05d86
EUNKYOUNG <amurca@naver.com>
2024-06-26 16:10:17 +0900

    1111

--- 2차 프로젝트/0.3구현소스/gnc-app/src/components/modules/PastArt.jsx
+++ 2차 프로젝트/0.3구현소스/gnc-app/src/components/modules/PastArt.jsx
@@ -1,65 +1,56 @@
-import React from 'react';
+import React from "react";
 
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 // CSS 연결
-import '../../css/pivot/pwork.scss';
+import "../../css/pivot/pwork.scss";
+
+// 데이터 불러오기
+import exData from "../data/exhibition_data";
 
 // import required modules
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
-
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
 //////////////////////////////////////
 
 export default function PastArt() {
-    return (
-      <>
-        <Swiper
-          spaceBetween={30}
-          centeredSlides={true}
-          autoplay={{
-            delay: 2500,
-            disableOnInteraction: false,
-          }}
-          pagination={{
-            clickable: true,
-          }}
-          navigation={true}
-          modules={[Autoplay, Pagination, Navigation]}
-          className="SwiperArt"
-        >
-          <SwiperSlide className='p-art'>Slide 1</SwiperSlide>
-          <SwiperSlide>Slide 2</SwiperSlide>
-          <SwiperSlide>Slide 3</SwiperSlide>
-          <SwiperSlide>Slide 4</SwiperSlide>
-          <SwiperSlide>Slide 5</SwiperSlide>
-          <SwiperSlide>Slide 6</SwiperSlide>
-          <SwiperSlide>Slide 7</SwiperSlide>
-          <SwiperSlide>Slide 8</SwiperSlide>
-          <SwiperSlide>Slide 9</SwiperSlide>
-          <SwiperSlide>Slide 10</SwiperSlide>
-          <SwiperSlide>Slide 11</SwiperSlide>
-          <SwiperSlide>Slide 12</SwiperSlide>
-          <SwiperSlide>Slide 13</SwiperSlide>
-          <SwiperSlide>Slide 14</SwiperSlide>
-          <SwiperSlide>Slide 16</SwiperSlide>
-          <SwiperSlide>Slide 17</SwiperSlide>
-          <SwiperSlide>Slide 18</SwiperSlide>
-          <SwiperSlide>Slide 19</SwiperSlide>
-          <SwiperSlide>Slide 20</SwiperSlide>
-          <SwiperSlide>Slide 21</SwiperSlide>
-          <SwiperSlide>Slide 22</SwiperSlide>
-          <SwiperSlide>Slide 23</SwiperSlide>
-          <SwiperSlide>Slide 24</SwiperSlide>
-          <SwiperSlide>Slide 25</SwiperSlide>
-          <SwiperSlide>Slide 26</SwiperSlide>
-        </Swiper>
-      </>
-    );
-  }
+  return (
+    <>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2200,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="SwiperArt"
+      >
+        {exData.map(
+          (v, i) =>
+            Number(v.idx) <= 26 && (
+              <SwiperSlide key={i}>
+                <div className="art-tit1">
+                  <img src={v.subimg} alt={v.mexhibi} />
+                </div>
+                <aside className="comp-tit2">
+                  <h1> Art 과거 전시회</h1>
+                  <h2>{v.mexhibi}</h2>
+                </aside>
+              </SwiperSlide>
+            )
+        )}
+      </Swiper>
+    </>
+  );
+}
