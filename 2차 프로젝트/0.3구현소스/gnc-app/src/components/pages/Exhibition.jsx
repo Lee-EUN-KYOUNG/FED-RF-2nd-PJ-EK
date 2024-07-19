c945f2fee375d732cbcb316c900d6cddbba60128
EUNKYOUNG <amurca@naver.com>
2024-07-19 17:38:50 +0900

    1111111

--- 2차 프로젝트/0.3구현소스/gnc-app/src/components/pages/Exhibition.jsx
+++ 2차 프로젝트/0.3구현소스/gnc-app/src/components/pages/Exhibition.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import ArtList from '../modules/ArtList';
+import { Link } from 'react-router-dom';
 
 
 
@@ -11,6 +12,31 @@ export default function Exhibition() {
   }, []);
 
   return (
+    <>
+    <button 
+    lassName='go-mbtn'
+    style={{
+      width: "100%",
+      height: "1vh",
+      fontSize: "1.2rem",
+      border: "none",
+      backgroundColor: "#fff",
+      paddingTop: "35px",
+      textAlign: "left",
+      paddingLeft: "31px"
+    }}
+    >
+      <Link to="/"
+      style={{
+        fontFamily: "Black Han Sans",
+        textDecoration: "none", 
+        color:"darkpurple",
+        fontSize: "1.2rem",
+      }}
+      >뒤로가기
+      </Link>
+    </button>
     <div id="art-showbx" ref={artgridRef}></div>
+    </>
   );
 }
