e9b851cebde053e0784b0cb7ad3c39bd8eb01a1b
EUNKYOUNG <amurca@naver.com>
2024-07-22 12:21:17 +0900

    111

--- 2차 프로젝트/0.3구현소스/gnc-app/src/index.js
+++ 2차 프로젝트/0.3구현소스/gnc-app/src/index.js
@@ -7,6 +7,7 @@ import Layout from "./components/layout/Layout";
 import Main from "./components/pages/Main";
 import Exhibition from "./components/pages/Exhibition";
 import ExhibiDetail from "./components/pages/ExhibiDetail";
+import SearchPage from "./components/pages/SearchPage";
 
 // 전체 공통 CSS 불러오기
 import "./css/pivot/index.scss";
@@ -21,6 +22,7 @@ export default function MainComponent() {
           <Route index element={<Main />} />
           <Route path="EXHIBITION" element={<Exhibition />} />
           <Route path="ExhibiDetail" element={<ExhibiDetail />} />
+          <Route path="search" element={<SearchPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
