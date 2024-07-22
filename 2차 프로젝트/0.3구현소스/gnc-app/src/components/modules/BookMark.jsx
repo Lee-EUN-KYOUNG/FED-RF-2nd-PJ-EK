cf6b31d7ef9065a243778eaf6566bd1fdf5f8fff
EUNKYOUNG <amurca@naver.com>
2024-07-22 16:23:39 +0900

    1111

---
+++ 2차 프로젝트/0.3구현소스/gnc-app/src/components/modules/BookMark.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+import { addComma } from "../func/common_fn";
+
+//컨텍스트 API 불러오기
+import { dCon } from "./dCon";
+
+// CSS 불러오기
+import "../../css/pivot/bookmark.scss";
+
+// 제이쿼리 불러오기
+import $ from "jquery";
+
+
+
+
+function BookMark(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default BookMark;
