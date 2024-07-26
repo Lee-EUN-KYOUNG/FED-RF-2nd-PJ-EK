4edbcfecee59f46f5ffae03d78546ae06920038d
EUNKYOUNG <amurca@naver.com>
2024-07-26 18:10:20 +0900

    111

--- 2차 프로젝트/0.3구현소스/gnc-app/src/components/modules/IntroPage.jsx
+++ 2차 프로젝트/0.3구현소스/gnc-app/src/components/modules/IntroPage.jsx
@@ -1,30 +1,28 @@
 /// 인트로 페이지
-import React from 'react';
+import React from "react";
 
 import "../../css/pivot/notice.scss";
-import MainSwiper from '../plugin/MainSwiper';
-import { Link, useNavigate } from 'react-router-dom';
-
+import MainSwiper from "../plugin/MainSwiper";
+import { Link, useNavigate } from "react-router-dom";
 
 function IntroPage(props) {
-
-    const goNav = useNavigate();
+  const goNav = useNavigate();
   const goPage = (seq) => {
-    goNav("/ExhibiDetail",{state:{page:seq}})
+    goNav("/ExhibiDetail", { state: { page: seq } });
   };
-  
-    return (
-        <>
-        <MainSwiper />
-        {/* <Link to="EXHIBITION"> */}
-        <span className='intro-imgtbox'>
-            <h1
-            onClick={()=>goPage(0)}
-            >자세히 보기</h1>
-        </span>
-        {/* </Link> */}
-        </>
-    );
+
+  return (
+    <>
+      <MainSwiper />
+      {/* <Link to="EXHIBITION"> */}
+      <span className="intro-imgtbox">
+        <h1 onClick={() => goPage(0)} style={{ cursor: "pointer" }}>
+          자세히 보기
+        </h1>
+      </span>
+      {/* </Link> */}
+    </>
+  );
 }
 
 export default IntroPage;
